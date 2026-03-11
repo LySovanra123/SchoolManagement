6fb6d2f3753f14f910118d50decec15ca77f063a
LySovanra123 <157804051+LySovanra123@users.noreply.github.com>
2026-03-11 16:39:56 +0700

    Finish Template Method

--- Form1.cs
+++ Form1.cs
@@ -20,26 +20,11 @@ namespace SchoolMangaement
             InitializeComponent();
         }
 
+
         private void btnLogin_Click(object sender, EventArgs e)
         {
-            SqlConnection con = Singleton.instance.GetConnection();
-
-            String username = txtUsername.Text;
-            String password = txtPassword.Text;
-            SqlCommand cnn = new SqlCommand("Select AdminName,AdminPassword from Admins where AdminName ='" + username + "' and AdminPassword='" + password + "'", con);
-            SqlDataAdapter da = new SqlDataAdapter(cnn);
-            DataTable dt = new DataTable();
-            da.Fill(dt);
-            if(dt.Rows.Count > 0) 
-            {
-                Main mn = new Main();
-                mn.Show();
-            }
-            else
-            {
-                MessageBox.Show("Invalid Username or Password");
-            }
-            con.Close();
+            TemplateMethod login = new RequestToAdmin();
+            login.TemplateMethodBase(txtUsername.Text, txtPassword.Text);
         }
 
         private void Form1_Load(object sender, EventArgs e)
---
+++ Pattern/TemplateMethod.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Documents.DocumentStructures;
+using System.Xml.Linq;
+using static System.Windows.Forms.VisualStyles.VisualStyleElement.StartPanel;
+
+namespace SchoolMangaement.Pattern
+{
+    public abstract class TemplateMethod
+    {
+        public void TemplateMethodBase(string name, string password)
+        {
+            bool success = SendQuery(name, password);
+            Result(success);
+        }
+
+        public abstract bool SendQuery(string name, string password);
+
+        public void Result(bool success)
+        {
+            if (success)
+            {
+                //this.Hide();
+                Main mn = new Main();
+                mn.Show();
+            }
+            else
+            {
+                MessageBox.Show("Invalid Username or Password");
+            }
+        }
+    }
+    public class RequestToAdmin : TemplateMethod
+    {
+        public override bool SendQuery(string name, string password)
+        {
+            SqlConnection con = Singleton.instance.GetConnection();
+
+            SqlCommand cmd = new SqlCommand(
+            "SELECT COUNT(*) FROM Admins WHERE AdminName=@name AND AdminPassword=@pass", con);
+
+            cmd.Parameters.AddWithValue("@name", name);
+            cmd.Parameters.AddWithValue("@pass", password);
+
+            int count = (int)cmd.ExecuteScalar();
+
+            return count > 0;
+        }
+    }
+
+}
